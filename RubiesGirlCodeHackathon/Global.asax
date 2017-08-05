4d6d1031092fc6afaa24afdc53098ed1e101f2df
Valerie Tshiani <tshval003@myuct.ac.za>
2017-08-05 12:48:52 +0200

    Add project files.

---
+++ RubiesGirlCodeHackathon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RubiesGirlCodeHackathon.MvcApplication" Language="C#" %>
---
+++ RubiesGirlCodeHackathon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RubiesGirlCodeHackathon.Startup))]
+namespace RubiesGirlCodeHackathon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
